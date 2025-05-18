6ed3b2e4f2ad301738fadeffbd5db1c2bb6b47b3
PrashantaDA <apras.dixit@gmail.com>
2025-05-18 22:29:57 +0545

    Added new images

--- src/utils/images.js
+++ src/utils/images.js
@@ -11,9 +11,10 @@ import join_image from "../assets/images/join_image.png";
 import game_details_image from "../assets/images/game_details_image.png";
 import loader from "../assets/images/loader.gif";
 import store_image from "../assets/images/store_image.png";
+import PA_bg from "../assets/images/PA_bg.jpg";
 
 // Array containing all slider images for use in carousels or sliders
 const sliderImages = [slider_img_1, slider_img_2, slider_img_3, slider_img_4, slider_img_5, slider_img_6, slider_img_7];
 
 // Exporting all images and arrays for use in other parts of the application
-export { banner_image, sliderImages, join_image, game_details_image, loader, store_image };
+export { banner_image, sliderImages, join_image, game_details_image, loader, store_image, PA_bg };
