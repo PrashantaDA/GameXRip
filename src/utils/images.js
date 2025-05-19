642e8ba6fddab11113672661ec71d2b85fba5291
PrashantaDA <apras.dixit@gmail.com>
2025-05-19 11:06:52 +0545

    Added new images

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,5 +1,8 @@
 // Importing image assets used throughout the application
+import loader from "../assets/images/loader.gif";
+
 import banner_image from "../assets/images/banner_image.png";
+
 import slider_img_1 from "../assets/images/slider_img_1.png";
 import slider_img_2 from "../assets/images/slider_img_2.jpg";
 import slider_img_3 from "../assets/images/slider_img_3.jpg";
@@ -7,15 +10,46 @@ import slider_img_4 from "../assets/images/slider_img_4.jpg";
 import slider_img_5 from "../assets/images/slider_img_5.jpg";
 import slider_img_6 from "../assets/images/slider_img_6.jpg";
 import slider_img_7 from "../assets/images/slider_img_7.jpg";
+import slider_img_8 from "../assets/images/slider_img_8.jpg";
+import slider_img_9 from "../assets/images/slider_img_9.jpg";
+import slider_img_10 from "../assets/images/slider_img_10.jpg";
+import slider_img_11 from "../assets/images/slider_img_11.png";
+import slider_img_12 from "../assets/images/slider_img_12.jpg";
+import slider_img_13 from "../assets/images/slider_img_13.jpg";
+import slider_img_14 from "../assets/images/slider_img_14.jpg";
+import slider_img_15 from "../assets/images/slider_img_15.jpg";
+import slider_img_16 from "../assets/images/slider_img_16.png";
+import slider_img_17 from "../assets/images/slider_img_17.jpg";
+import slider_img_18 from "../assets/images/slider_img_18.jpg";
+
 import join_image from "../assets/images/join_image.png";
 import game_details_image from "../assets/images/game_details_image.png";
-import loader from "../assets/images/loader.gif";
 import store_image from "../assets/images/store_image.png";
+
 import PA_bg from "../assets/images/PA_bg.jpg";
 import MK_bg from "../assets/images/MK_bg.jpg";
 
 // Array containing all slider images for use in carousels or sliders
-const sliderImages = [slider_img_1, slider_img_2, slider_img_3, slider_img_4, slider_img_5, slider_img_6, slider_img_7];
+const sliderImages = [
+	slider_img_1,
+	slider_img_2,
+	slider_img_3,
+	slider_img_4,
+	slider_img_5,
+	slider_img_6,
+	slider_img_7,
+	slider_img_8,
+	slider_img_9,
+	slider_img_10,
+	slider_img_11,
+	slider_img_12,
+	slider_img_13,
+	slider_img_14,
+	slider_img_15,
+	slider_img_16,
+	slider_img_17,
+	slider_img_18,
+];
 
 // Exporting all images and arrays for use in other parts of the application
 export { banner_image, sliderImages, join_image, game_details_image, loader, store_image, PA_bg, MK_bg };
